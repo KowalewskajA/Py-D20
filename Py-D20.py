fc5cde08796fb06854386cea352af90cc5f4771c
Kowalewskaja <Alexander.Lahn.BSc@googlemail.com>
2021-10-07 09:24:40 +0200

    Refactored to use the Roll class

--- Py-D20.py
+++ Py-D20.py
@@ -4,74 +4,86 @@ from random import randint
 
 class Roll():
 	def __init__(self, str):
+		#3d12+4d6-2d2
 		self.str = str.replace(" ", "")
 		self.dPos = [_.start() for _ in re.finditer("d", str)]
 		self.opPos = [_.start() for _ in re.finditer("[+|-]", str)]
+
 		#If calculation can not be done abort
-		if len(opPos) + 1 != len(dPos):
+		if len(self.opPos) + 1 != len(self.dPos):
 			print("Error: not matching number of dice and operators!", file=sys.stderr)
 			return False
 
 		self.dice = []
+		self.setDice()
 		self.factor = []
-		self.opPos.append(len(str))
+		self.setFactor()
 
-		#Generate the dice values and save them
-		for i in range(len(opPos)):
-			dice.append(int(str[dPos[i]+1:opPos[i]]))
-
+	def setDice(self):
 		#Prepare the opPos for simpler arithmetic
-		opPos.remove(len(str))
-		opPos.append(-1)
-		opPos.sort()
+		self.opPos.append(len(str))
+		#Generate the dice values and save them
+		for i in range(len(self.opPos)):
+			self.dice.append(int(str[self.dPos[i]+1:self.opPos[i]]))
+		#reset
+		self.opPos.remove(len(str))
+
+	def setFactor(self):
+		#Prepare the opPos for simpler arithmetic
+		self.opPos.append(-1)
+		self.opPos.sort()
 
 		#Generate the factors and save them
-		for i in range(len(opPos)):
-			start = opPos[i]+1
-			end = dPos[i]
+		for i in range(len(self.opPos)):
+			start = self.opPos[i]+1
+			end = self.dPos[i]
 			if start == end:
-				factor.append(1)
+				self.factor.append(1)
 			else:
-				factor.append(int(str[start:end]))
+				self.factor.append(int(str[start:end]))
+		#reset
+		self.opPos.remove(-1)
 
-		opPos.remove(-1)
-
+	def calculateRoll(self):
 		#Calculate the result
 		_tmp = 0
-		for i in range(len(dPos)):
+		for i in range(len(self.dPos)):
 			if i == 0:
-				_tmp = factor[i] * calculateDice(dice[i])
+				for j in range(self.factor[i]):
+					_tmp = _tmp + calculateDice(self.dice[i])
 			else:
-				if str[opPos[i-1]] == "+":
-					_tmp = _tmp + factor[i] * calculateDice(dice[i])
-				else:
-					_tmp = _tmp - factor[i] * calculateDice(dice[i])
-		#print(f'opPos[]: {opPos}')
-		#print(f'dice[]: {dice}')
-		#print(f'factor[]: {factor}')
+				for j in range(self.factor[i]):
+					if str[self.opPos[i-1]] == "+":
+						_tmp = _tmp + calculateDice(self.dice[i])
+					else:
+						_tmp = _tmp - calculateDice(self.dice[i])
 		return _tmp
 
-def processDiceString(str):
-	#3d12+4d6-2d2
-
-	#debug
-	#print(f'len(str): {len(str)}')
-	#print(f'dPos[]: {dPos}')
-	#print(f'opPos[]: {opPos}')
-
+	def printRoll(self, cached=False):
+		res1 = self.calculateRoll()
+		res2 = self.calculateRoll()
+		if cached == True:
+			print(f"Cached {str}: (1){res1} (2){res2}")
+		else:
+			print(f"{str}: (1){res1} (2){res2}")
 
 def calculateDice(diceSize):
 	return randint(1, diceSize)
 
 if __name__ == '__main__':
+	rolls = {}
 	exit = False
 	while not exit:
 		str = input(">")
 		if str == "exit":
 			exit = True
 		else:
-			print(str)
-			res1 = processDiceString(str)
-			if res1 != False:
-				res2 = processDiceString(str)
-				print(f"{str}: (1){res1} (2){res2}")
+			#We encountered the string allready hence we use the cached object
+			if str in rolls.keys():
+				rolls[str].printRoll(cached=True)
+			#We did not encountered the string and create a new
+			else:
+				roll = Roll(str)
+				if roll != False:
+					rolls[str] = roll
+					roll.printRoll()
--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Py-D20
 
 Is a short commandline tool that evaluates dice strings that you would find in an DnD related calculations.
+
+# Input
+
+It processes:
+sum:=expression{+|-}expression
+expression:={number|''}d{number}
+
+# Example
+
+If you provite the same expression more than one the cache Object will be reused, which will be mentioned in the output.
+
+```console
+foo@bar:~$ py Py-D20.py
+>3d12+4d6-2d2
+3d12+4d6-2d2: (1)42 (2)42
+>3d12+4d6-2d2
+Cached 3d12+4d6-2d2: (1)36 (2)33
+>3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2                
+Cached 3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2+3d12+4d6-2d2: (1)330 (2)307
+```
+
+# Potential improvement
+
+- [ ] "> expression name", so you can call it 'axe' for example and use it afterwards as "> axe"
+- [ ] "> list", so you can see which expression you have saved with what references
+- [ ] capabilty to save your expressions in a file
+- [ ] capabilty to load your expressions from a file
+
